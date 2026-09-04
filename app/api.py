d7433c1fbcb287eae27948e120c837cf255f44a4
Atharva Kulkarni <kulkarniatharva118@gmail.com>
2026-09-05 04:55:35 +0530

    CORS

--- app/api.py
+++ app/api.py
@@ -30,7 +30,7 @@ app.add_middleware(
         FRONTEND_URL,
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://settlement-q-a-agent-for-fintech-su.vercel.app/",
+        "https://settlement-q-a-agent-for-fintech-su.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
